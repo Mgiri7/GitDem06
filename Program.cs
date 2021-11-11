85a4dd5d14ffe106bf640298ecca43549bfa6664
giri <girim7593@gmail.com>
2021-11-11 21:43:07 +0530

    New Changes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace GitDemo6
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello it's me");
+            Console.WriteLine("Apples");
         }
     }
 }
